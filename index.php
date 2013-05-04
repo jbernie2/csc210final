57f539ef55978f0420a9c6ef64030919a21d8c33
jbernie2 <jbernie2@u.rochester.edu>
2013-05-04 21:27:00 +0000

    created a registration page, and a form validation file

--- index.php
+++ index.php
@@ -1,29 +1,16 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN" "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
-
-        <head>
-                <title>Hello World</title>
-
-                <meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+	<head>
+		<title>Hello World</title>
+		<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
 	</head>
 	<body>
 		<h1>HELLO WORLD!</h1>
 		<p>No stealing!</p>
-
-
 		<form name="login" action="login.php" method="post">
     			Email: <input type="text" name="email" />
     			Password: <input type="password" name="password" />
     		<input type="submit" value="Login" />
-		</form>
-	
-		<form name="register" action="register.php" method="post">
-    			First Name: <input type="text" name="first_name" maxlength="30" />
-				Last Name: <input type="text" name="last_name" maxlength="30" />
-    			Email: <input type="text" name="email" maxlength="50" />
-				Password: <input type="password" name="pass1" />
-   				Password Again: <input type="password" name="pass2" />
-    		<input type="submit" value="Register" />
 		</form>
 	</body>
 </html>
