c0ddb34799191903e8fce2c41bae732440f94917
root <root@ip-10-196-107-136.ec2.internal>
2013-05-05 19:55:24 +0000

    validation is working:

--- js/validate.js
+++ js/validate.js
@@ -1,21 +1,30 @@
-alert("js is hooked up!");
-
 $(document).ready(function(){
-	alert("All up in this bitch");
-	
+	//alert("value time: " + $("#first_name").val);
+	//alert("Here is the first name: " + $("#first_name"));
 	// let's set up some variables
-	var first_name = $("#first_name");
+	/*var first_name = $("#first_name");
 	var last_name = $("#last_name");
 	var email = $("#email");
 	var age = $("#age");
-	var gender = $("#gender");	
+	var gender = $("#gender");*/	
 		
 
 	$("#register").submit(function(){
-		if(validateFirstName() && validateLastName() && validateEmail() && validateAge())
+
+	var first_name = document.getElementById("first_name").value;
+	var last_name = document.getElementById("last_name").value;
+	var email = document.getElementById("email").value;
+	var age = document.getElementById("age").value;
+	//var gender = $("#gender");	
+	//alert("here is the fname: " + first_name);
+		if(validateFirstName() && validateLastName() && validateEmail() && validateAge()){
+			alert("The data checks out. Welcome!");
 			return true;
-		else
+		}
+		else{
+			alert("Your data was the derps. Not added. Try again.");
 			return false;
+		}
 	});
 
 	// stuff to use in the methods below
@@ -24,8 +33,8 @@ $(document).ready(function(){
 
 	// validating functions
 	function validateFirstName(){
-		alert("checking first name");
 		if(first_name.value.match(letters)){
+			alert("first name good!");
 			return true;
 		}
 		else{
@@ -34,7 +43,9 @@ $(document).ready(function(){
 	}
 
 	function validateLastName(){
-		if(last_name.value.match(letters){
+		//alert("checking last name");
+		if(last_name.value.match(letters)){
+			alert("last name good!");
 			return true;
 		}
 		else{
@@ -43,7 +54,9 @@ $(document).ready(function(){
 	}
 
 	function validateEmail(){
-		if(email.value.match(emailFormat){
+		//alert("checking email");
+		if(email.value.match(emailFormat)){
+			alert("email is good!");
 			return true;
 		}
 		else{
@@ -52,8 +65,16 @@ $(document).ready(function(){
 	}
 
 	function validateAge(){
-		var n = ~~Number(age);
-		return String(n) === age && n >= 0;
+		//alert("checking age " + age);
+		if(!isNaN(age.value)){
+			alert("age is good!");
+			return true;
+		}
+		else{
+			alert("this is the bad age:" + age.value);
+			return false;
+		}
 	}
+
 
 });
