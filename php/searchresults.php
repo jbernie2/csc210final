ee88ebf6234ee8bfc7bb799bd4d3260e0071a73d
Joe <jcolaruo@u.rochester.edu>
2013-05-04 21:07:06 -0400

    search results spit into table

--- php/searchresults.php
+++ php/searchresults.php
@@ -10,7 +10,6 @@
 	<?php
 		ini_set('display_errors', 1); 
 		error_reporting(E_ALL);
-		include 'db.php';
 	
 		$school = htmlspecialchars(mysql_real_escape_string($_REQUEST["school"]));
 		$degree = $_REQUEST["degree"];
