449b7d394adeeb77e56a976138912417661bcdf9
jbernie2 <jbernie2@u.rochester.edu>
2013-05-07 19:55:56 +0000

    added name to top of page

--- php/banner/user_panel.php
+++ php/banner/user_panel.php
@@ -1,4 +1,5 @@
         <div id="user_panel">
+		 <div id="username"><?php echo "hello, ".$_SESSION['first_name']; ?> </div>
        	 <div id="logout"><a href="../php/logout.php">Logout</a></div>
         </div>
 
