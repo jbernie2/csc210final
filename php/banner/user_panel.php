9f4bc9d75556e036954731cc5b5053d5d8a53ea3
Emily Danchik <ecdanchik@gmail.com>
2013-05-07 20:01:18 +0000

    styled the username at the top right

--- php/banner/user_panel.php
+++ php/banner/user_panel.php
@@ -1,5 +1,5 @@
         <div id="user_panel">
-		 <div id="username"><?php echo "hello, ".$_SESSION['first_name']; ?> </div>
+		 <div id="username"><?php echo "Hello, ".$_SESSION['first_name']."."; ?> </div>
        	 <div id="logout"><a href="../php/logout.php">Logout</a></div>
         </div>
 
