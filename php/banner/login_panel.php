90bc651606f124774818aaf7de09bf13ee492cae
Emily Danchik <ecdanchik@gmail.com>
2013-05-07 18:07:55 +0000

    fixing that merge

--- php/banner/login_panel.php
+++ php/banner/login_panel.php
@@ -5,12 +5,7 @@
                         <input type="text" name="email" placeholder="E-mail"/>
                         <input type="password" name="password" placeholder="Password"/>
                         <input type="submit" value="Login" />
-<<<<<<< HEAD
                         </form>
                         <a href="../html/register.html" id="register">Register an account</a>
-=======
-                        <a href="../html/register.html">Register an account</a>
-			</form>
->>>>>>> 07b3936bc5b826ea2c4679d35a63c0b086d74d59
         </div>
 
