6828d79033501a2d57f9da170eb916ab557243cf
jbernie2 <jbernie2@u.rochester.edu>
2013-05-07 18:12:14 +0000

    multiple removes now work

--- php/updatePrograms.php
+++ php/updatePrograms.php
@@ -13,11 +13,13 @@
 	$results = array();
 
 	foreach($_POST as $fieldName => $value){
-		
+	
 		$fieldName = mysql_real_escape_string($fieldName);
 		$value = mysql_real_escape_string($value);
 
-		if($fieldName == "remove"){
+		//check if the user wants to remove a program
+		$extractedField = strstr($fieldName,"remove");
+		if($extractedField == "remove"){
 			$user_id = $_SESSION['user_id'];
 			mysql_query("DELETE from proglookup
 							where(user_id='$user_id' AND program_id='$value')
@@ -28,10 +30,10 @@
 			//I encoded the id of the school into the id of
 			//the form field in the html form
 			//now I am extracting the school id and the sql field
-			$fieldName = explode("_",$fieldName,2);
+			$splitField = explode("_",$fieldName,2);
 
-			$program_id = $fieldName[0];
-			$fieldName = $fieldName[1];
+			$program_id = $splitField[0];
+			$fieldName = $splitField[1];
 
 			//update info in the mysql table for what a user has submitted 
 			//to a particular school
