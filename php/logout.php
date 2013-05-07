a690c6b0f6ed7f9ca5e972dbf8b68258659b14b0
Emily Danchik <ecdanchik@gmail.com>
2013-05-07 04:40:32 +0000

    Now, logging out will not send the user to my server

--- php/logout.php
+++ php/logout.php
@@ -1,6 +1,6 @@
 <?php
 	session_start();
 	session_destroy(); 	
-	echo "<script>window.location = 'http://ec2-23-20-127-16.compute-1.amazonaws.com/csc210final/php/'</script>";
+	echo "<script>window.location = '../php/'</script>";
 	
 ?>
